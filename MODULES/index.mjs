81aa988c9c696370d9b4e46adfbf6ddbf70bc34d
Dhrubajyoti-Pramanick <dhrubajyoti@itobuz.com>
2023-03-03 18:45:32 +0530

    done my math module

---
+++ MODULES/index.mjs
@@ -0,0 +1,12 @@
+// const math = require('./math');
+// import {add, sub} from './math.mjs';
+import * as calc from './math.mjs';
+
+// console.log(math.add(1, 3));
+// console.log(math.sub(1, 3));
+console.log(calc.add(1,3));
+console.log(calc.sub(1,3));
+
+
+// import math from './math.mjs';
+// console.log(math);
---
+++ MODULES/math.mjs
@@ -0,0 +1,20 @@
+// export default function add() {
+//     return 1;
+// }
+
+export  function add(a, b) {
+    return a + b;
+}
+export function sub(a, b) {
+    return a - b;
+}
+
+// export default {
+//     add: add,
+// }
+//common js
+// module.exports = {
+//     add : add,
+//     sub : sub
+// }
+
